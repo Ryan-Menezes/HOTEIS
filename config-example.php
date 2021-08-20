c27aa20f7d443444dc42532d1217dcfabe5357c8
Ryan de Menezes <menezesryan1010@gmail.com>
2021-08-20 18:24:09 -0300

    Atualizando o config-example.php

--- config-example.php
+++ config-example.php
@@ -1,18 +1,18 @@
 <?php
 // CONFIGURAÇÕES DO SITE
 
-define('TIMEZONE', 'America/Sao_Paulo');								// TIMEZONE DA APLICAÇÃO
-define('ERROR_REPORTING', E_ALL);										// APRESENTAÇÃO DE ERROS(PARA NÃO APRESENTAR ERROS NA TELA, BASTA COLOCAR UM 0)
+define('TIMEZONE', 'America/Sao_Paulo');													// TIMEZONE DA APLICAÇÃO
+define('ERROR_REPORTING', E_ALL);															// APRESENTAÇÃO DE ERROS(PARA NÃO APRESENTAR ERROS NA TELA, BASTA COLOCAR UM 0)
 
 // URLS
 
-define('URL', 'http://exemple');										// URL DO SISTEMA
-define('URL_HOME', URL . 'public/');									// CAMINHO DA PÁGINA HOME
-define('URL_PAINEL', URL_HOME . 'PAINEL/');								// CAMINHO DA PÁGINA DE DASHBOARD DO SISTEMA
+define('URL', 'http://www.hoteis.com/');													// URL DO SISTEMA
+define('URL_HOME', URL);																	// CAMINHO DA PÁGINA HOME
+define('URL_PAINEL', URL_HOME . 'PAINEL/');													// CAMINHO DA PÁGINA DE DASHBOARD DO SISTEMA
 
 // CONFIGURAÇÕES DO SISTEMA
 
-define('CONFIGJSON', 'config.json');									// ARQUIVO DE CONFIGURAÇÕES(.json) CONTENDO MAIS ALGUMAS INFORMAÇÕES DO SISTEMA
+define('CONFIGJSON', 'config.json');														// ARQUIVO DE CONFIGURAÇÕES(.json) CONTENDO MAIS ALGUMAS INFORMAÇÕES DO SISTEMA
 
 $CONFIG = json_decode(file_get_contents(URL . CONFIGJSON));
 
@@ -49,8 +49,8 @@ define('DB_CHARSET', 'utf8');
 // CONFIGURAÇÕES DO EMAIL
 
 define('EMAIL_SERVER', 'smtp.gmail.com');
-define('EMAIL_FROM', '');												// EMAIL ONDE IRÁ RECEBER E ENVIAR MENSAGENS
-define('EMAIL_PASS', '');												// SENHA DO E-MAIL
+define('EMAIL_FROM', '');																	// EMAIL ONDE IRÁ RECEBER E ENVIAR MENSAGENS
+define('EMAIL_PASS', '');																	// SENHA DO E-MAIL
 define('EMAIL_PORT', '587');
 
 // OPÇÕES DE ROTA
@@ -65,16 +65,16 @@ define('MAX_FILE_SIZE', '1048576');
 
 // CONFIGURAÇÕES DO CHECKOUT SEGURO DO PAYPAL
 
-define('MODE', 'sandbox'); 												// MODO DE PAGAMENTO DO PAYPAL(sandbox - somente para testes | live - para pagamentos reais)
+define('MODE', 'sandbox'); 																	// MODO DE PAGAMENTO DO PAYPAL(sandbox - somente para testes | live - para pagamentos reais)
 
 if(MODE === 'sandbox'):
-	define('PAYMENT_URL', 'https://api-m.sandbox.paypal.com/');			// URL ONDE SERÁ FEITA TODAS AS REQUISIÇÕES SSL PARA A API DO PAYPAL NO MODO sandbox
-	define('PAYMENT_CLIENTID', '');										// ESTE ID É FORNECIDO PELO PAYPAL PARA O MODO sandbox
-	define('PAYMENT_SECRETKEY', '');									// ESTA CHAVE É FORNECIDA PELO PAYPAL PARA O MODO sandbox
+	define('PAYMENT_URL', 'https://api-m.sandbox.paypal.com/');								// URL ONDE SERÁ FEITA TODAS AS REQUISIÇÕES SSL PARA A API DO PAYPAL NO MODO sandbox
+	define('PAYMENT_CLIENTID', '');															// ESTE ID É FORNECIDO PELO PAYPAL PARA O MODO sandbox
+	define('PAYMENT_SECRETKEY', '');														// ESTA CHAVE É FORNECIDA PELO PAYPAL PARA O MODO sandbox
 else:
-	define('PAYMENT_URL', 'https://api-m.paypal.com/');					// URL ONDE SERÁ FEITA TODAS AS REQUISIÇÕES SSL PARA A API DO PAYPAL NO MODO live
-	define('PAYMENT_CLIENTID', '');										// ESTE ID É FORNECIDO PELO PAYPAL PARA O MODO live
-	define('PAYMENT_SECRETKEY', '');									// ESTA CHAVE É FORNECIDA PELO PAYPAL PARA O MODO live
+	define('PAYMENT_URL', 'https://api-m.paypal.com/');										// URL ONDE SERÁ FEITA TODAS AS REQUISIÇÕES SSL PARA A API DO PAYPAL NO MODO live
+	define('PAYMENT_CLIENTID', '');															// ESTE ID É FORNECIDO PELO PAYPAL PARA O MODO live
+	define('PAYMENT_SECRETKEY', '');														// ESTA CHAVE É FORNECIDA PELO PAYPAL PARA O MODO live
 endif;
 
 define('PAYMENT_EMAIL', $CONFIG->payment->email);
